9c248a431fcddf2df56d96165e71795d08d6c8e1
satyam1812 <satyam300696@gmail.com>
2018-04-10 18:18:44 +0530

    uodate

--- controllers/user.js
+++ controllers/user.js
@@ -13,46 +13,46 @@ let {
 } = require('age-calculator');
 
 /* Controller for user login */
-// exports.login = function(req, res) {
-// 	var { email, password, device_type, device_token, latitude, longitude } = req.body;
+ exports.login = function(req, res) {
+ 	var { email, password, device_type, device_token, latitude, longitude } = req.body;
 
-// 	var manValues = [email, password, device_type, device_token, latitude, longitude];
-// 	var checkBlank = commfunc.checkBlank(manValues);
-// 	if (checkBlank == 1) {
-// 		responses.parameterMissing(res);
-// 	} else {
-// 		UserModal.checkLoginEmail(email, function(result){
-// 			responses.sendError(res);
-// 			} else if( result == 2 ) {
-// 				console.log("emailNotFound"); 
-// 			} else {
-// 				var encry_hash = md5(password);
-// 				if (result[0].password != '53f0ab092c55f279b7a8b8f297e8ac89') {
-// 					console.log("Invalid password");
-// 				} else {
+ 	var manValues = [email, password, device_type, device_token, latitude, longitude];
+ 	var checkBlank = commfunc.checkBlank(manValues);
+ 	if (checkBlank == 1) {
+ 		responses.parameterMissing(res);
+ 	} else {
+ 		UserModal.checkLoginEmail(email, function(result){
+ 			responses.sendError(res);
+ 			} else if( result == 2 ) {
+ 				console.log("emailNotFound"); 
+ 			} else {
+ 				var encry_hash = md5(password);
+ 				if (result[0].password != '53f0ab092c55f279b7a8b8f297e8ac89') {
+ 					console.log("Invalid password");
+ 				} else {
 
-// 					var access_token = md5(new Date());
-// 					var data = {access_token: access_token, device_type: device_type, device_token: device_token, latitude: latitude, longitude: longitude};
-// 					var condition = {user_id: result[0].user_id};
-
-// 					UserModal.updateUserData(data, condition, function(updatedResult){
-// 						if (updatedResult == 0) {
-// 							responses.sendError(res);
-// 						} else {
-// 							result[0].access_token = access_token;
-// 							result[0].device_type = device_type;
-// 							result[0].device_token = device_token;
-// 							result[0].latitude = latitude;
-// 							result[0].longitude = longitude;
-// 							result[0].password = "";
-// 							responses.success(res, result[0]);
-// 						}
-// 					});
-// 				}
-// 			}
-// 		});
-// 	}
-// }
+ 					var access_token = md5(new Date());
+ 					var data = {access_token: access_token, device_type: device_type, device_token: device_token, latitude: latitude, longitude: longitude};
+   					var condition = {user_id: result[0].user_id};
+ 
+ 					UserModal.updateUserData(data, condition, function(updatedResult){
+ 						if (updatedResult == 0) {
+ 							responses.sendError(res);
+ 						} else {
+ 							result[0].access_token = access_token;
+ 							result[0].device_type = device_type;
+ 							result[0].device_token = device_token;
+ 							result[0].latitude = latitude;
+ 							result[0].longitude = longitude;
+ 							result[0].password = "";
+ 							responses.success(res, result[0]);
+ 						}
+ 					});
+ 				}
+ 			}
+ 		});
+ 	}
+ }
 
 exports.signup = function(req, res) {
     var {
